409c2c72afc6f2bab6fe06cf6b6f1ce49f5b18da
Marco Ferreira <marcoavfcc@gmail.com>
2021-03-14 23:55:22 -0300

    finally

--- src/App.js
+++ src/App.js
@@ -5,37 +5,14 @@ import ListaDeNotas from './components/ListaDeNotas';
 import './assets/App.css';
 import './assets/index.css';
 import ListaDeCategorias from './components/ListaDeCategorias';
+import Categorias from './dados/Categorias';
+import ArrayDeNotas from './dados/Notas';
 
 class App extends Component {
   constructor() {
-    super()
-  
-    this.state = {
-      notas: [],
-      categorias: ["Games", "Música"],
-    }
-  };
-
-  adicionarCategoria(nomeCategoria) {
-    const novoArrayCategorias = [...this.state.categorias, nomeCategoria] 
-    const novoEstado = {...this.state, categorias: novoArrayCategorias};
-    this.setState(novoEstado);
-  }
-
-  criarNota(titulo, texto, categoria) {
-    const novaNota = { titulo, texto, categoria };
-    const novoArrayNotas = [...this.state.notas, novaNota]
-    const novoEstado = {
-      notas: novoArrayNotas
-    }
-    this.setState(novoEstado);
-  }
-
-  deletarNota(index){
-    let arrayNotas = this.state.notas;
-    arrayNotas.splice(index,1);
-    this.setState({ notas: arrayNotas })
-    console.log('deletar')
+    super();
+    this.categorias = new Categorias();
+    this.notas = new ArrayDeNotas();
   }
 
   render() {
@@ -43,17 +20,17 @@ class App extends Component {
     return (
       <section className="Conteudo">
         <FormularioCadastro
-          categorias={this.state.categorias} 
-          criarNota={this.criarNota.bind(this)} 
+          categorias={this.categorias} 
+          criarNota={this.notas.adicionarNota.bind(this.notas)} 
         />
         <main className="conteudo-principal">
           <ListaDeCategorias 
-            adicionarCategoria={this.adicionarCategoria.bind(this)}
-            categorias={this.state.categorias}
+            adicionarCategoria={this.categorias.adicionarCategorias.bind(this.categorias)}
+            categorias={this.categorias}
           />
           <ListaDeNotas
-            apagarNota={this.deletarNota.bind(this)} 
-            notas={this.state.notas} 
+            apagarNota={this.notas.apagarNota.bind(this.notas)} 
+            notas={this.notas} 
           />
         </main>
         
--- src/components/FormularioCadastro/index.jsx
+++ src/components/FormularioCadastro/index.jsx
@@ -8,6 +8,21 @@ export default class FormularioCadastro extends Component {
         this.titulo = "";
         this.texto = "";
         this.categoria = "Sem Categoria";
+        this.state = {categorias: []}
+
+        this._novasCategorias = this._novasCategorias.bind(this);
+    }
+
+    componentDidMount() {
+        this.props.categorias.inscrever(this._novasCategorias);
+    }
+
+    componentWillUnmount() {
+        this.props.categorias.desinscrever(this._novasCategorias);
+    }
+
+    _novasCategorias(categorias) {
+        this.setState({...this.state, categorias})
     }
 
     _handlerMudancaCategoria(event) {
@@ -42,8 +57,8 @@ export default class FormularioCadastro extends Component {
                 >
                     <option>Sem Categoria</option>
 
-                    {this.props.categorias.map(categoria => {
-                        return <option>{categoria}</option>
+                    {this.state.categorias.map((categoria, index) => {
+                        return <option key={index}>{categoria}</option>
                     })}
                 </select>
                 <input 
